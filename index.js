4eed2fc3be277b8cc290d15c25e9a3fb2c6d453f
Dean Felch <deanfelch@gmail.com>
2025-07-02 15:39:53 -0700

    Add CORS support for frontend

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const multer = require("multer");
 const axios = require("axios");
 const FormData = require("form-data");
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*",  // or "*" for development
+}));
 
 const app = express();
 const upload = multer();
