879c7732c16b8ca8f81c211f55855a9e6c76afe1
Dean Felch <deanfelch@gmail.com>
2025-07-03 10:45:11 -0700

    Change DB to MYSQL

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
 
 require("dotenv").config();
 const express = require("express");
